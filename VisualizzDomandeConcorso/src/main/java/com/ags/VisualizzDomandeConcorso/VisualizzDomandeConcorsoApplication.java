93bdd5ab2946bca3fb7be3e60c341597da881a5d
agsuser <agsuser@192.168.1.102>
2018-10-18 12:20:02 +0200

    Commit base

---
+++ VisualizzDomandeConcorso/src/main/java/com/ags/VisualizzDomandeConcorso/VisualizzDomandeConcorsoApplication.java
@@ -0,0 +1,12 @@
+package com.ags.VisualizzDomandeConcorso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizzDomandeConcorsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizzDomandeConcorsoApplication.class, args);
+	}
+}
