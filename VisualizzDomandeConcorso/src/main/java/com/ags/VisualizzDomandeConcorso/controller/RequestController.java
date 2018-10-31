5d1691e54df5cab66bb6c3fd95c24af96782c7bf
agsuser <agsuser@milwksxxxl>
2018-10-31 17:31:28 +0100

    select works without th:field

--- VisualizzDomandeConcorso/src/main/java/com/ags/VisualizzDomandeConcorso/controller/RequestController.java
+++ VisualizzDomandeConcorso/src/main/java/com/ags/VisualizzDomandeConcorso/controller/RequestController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.ags.VisualizzDomandeConcorso.model.DegreeEducation;
 import com.ags.VisualizzDomandeConcorso.service.DegreeEducationService;
 
 @Controller
@@ -18,10 +21,16 @@ public class RequestController {
 	}
 	
 	@GetMapping("/request")
-	public String getRequest(Model model) {
+	public String getEduList(Model model) {
 		model.addAttribute("educations", eduService.getEducations());
 		return "requestForm";
 	}
+	
+//	@PostMapping("/request/")
+//	public String getQualificationList(Model model, @ModelAttribute DegreeEducation degreeEducation) {
+//		return null;
+//	}
+	
 	
 //	@GetMapping("/request/edu")
 //	public String educationsList(Model model) {
---
+++ VisualizzDomandeConcorso/src/main/java/com/ags/VisualizzDomandeConcorso/view/bean/RequestBean.java
@@ -0,0 +1,13 @@
+package com.ags.VisualizzDomandeConcorso.view.bean;
+
+public class RequestBean {
+	private String selectedDegree;
+
+	public String getSelectedDegree() {
+		return selectedDegree;
+	}
+
+	public void setSelectedDegree(String selectedDegree) {
+		this.selectedDegree = selectedDegree;
+	}
+}
